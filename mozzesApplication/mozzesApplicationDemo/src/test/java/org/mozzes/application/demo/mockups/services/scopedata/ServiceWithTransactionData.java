fa5f89d6eddeb8e70c36bd2e013e65db327c06a5
Branko Kokanovic <branko.kokanovic@gmail.com>
2010-02-27 14:56:25 +0000

    applied LGPL licence to all files

--- mozzesApplication/mozzesApplicationDemo/src/test/java/org/mozzes/application/demo/mockups/services/scopedata/ServiceWithTransactionData.java
+++ mozzesApplication/mozzesApplicationDemo/src/test/java/org/mozzes/application/demo/mockups/services/scopedata/ServiceWithTransactionData.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2010 Mozzart
+ *
+ *
+ * This file is part of mozzes.
+ *
+ * mozzes is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * mozzes is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with mozzes.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 package org.mozzes.application.demo.mockups.services.scopedata;
 
 import org.mozzes.application.common.transaction.*;
