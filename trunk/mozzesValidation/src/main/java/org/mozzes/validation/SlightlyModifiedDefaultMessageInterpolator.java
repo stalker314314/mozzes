d5f30a68edb31b726ea7a9b7caf2d5e292b2227c
branko.kokanovic <branko.kokanovic@d68a0d0a-2081-11df-8328-bd766baa8347>
2010-02-27 14:56:25 +0000

    applied LGPL licence to all files

--- trunk/mozzesValidation/src/main/java/org/mozzes/validation/SlightlyModifiedDefaultMessageInterpolator.java
+++ trunk/mozzesValidation/src/main/java/org/mozzes/validation/SlightlyModifiedDefaultMessageInterpolator.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2010 Mozzart
+ *
+ *
+ * This file is part of mozzes.
+ *
+ * mozzes is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * mozzes is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with mozzes.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 // $Id: ResourceBundleMessageInterpolator.java 17620 2009-10-04 19:19:28Z hardy.ferentschik $
 package org.mozzes.validation;
 
